2dcdcca6395819c176688c4aebdce0026325656f
Ayodele Temitope <dyenamics228@gmail.com>
2025-03-18 13:51:41 +0100

    add box shadow with the appropriate color

--- src/components/JobCards.tsx
+++ src/components/JobCards.tsx
@@ -9,7 +9,7 @@ interface JobCardProps {
 const JobCard: React.FC<JobCardProps> = ({ job, addFilter }) => {
   return (
     <div
-      className={`py-8 px-4 rounded-sm shadow-md relative ${
+      className={`py-8 px-4 rounded-sm myBox relative ${
         job.featured ? "border-l-4 border-Desaturated-Dark-Cyan" : ""
       }`}
     >
